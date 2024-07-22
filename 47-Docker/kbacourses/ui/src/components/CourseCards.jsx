e1455b07c086010ca036f829d3b7fbf753a3c4cd
nirajajshenoy <157449412+nirajajshenoy@users.noreply.github.com>
2024-07-22 10:13:25 +0530

    Add files via upload

---
+++ 47-Docker/kbacourses/ui/src/components/CourseCards.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+import CourseCard from './CourseCard'
+// import courses from '../courses.json'
+import {BarLoader} from 'react-spinners'
+
+
+const CourseCards = ({ isHome = false }) => {
+
+    const [courses, setCourses] = useState([]);
+    const [loading, setLoading] = useState(true)
+    // console.log(courses)
+    useEffect(() => {
+  
+        const fetchCourses = async () => {
+            try {
+                const res = await fetch('/api/courses');
+                const data = await res.json()
+                setCourses(data)
+            } catch (error) {
+                console.log('error', error)
+            } finally {
+                setLoading(false)
+            }
+        };
+        fetchCourses()
+    },[])
+    const courseList = isHome ? courses.slice(0, 3) : courses;
+    console.log("courses===",courses)
+    return (
+        <>
+            <h1>{isHome ? 'Top courses' : 'All courses'}</h1>
+            {loading ? <BarLoader /> : <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 mx-5 my-10'>
+                {courseList.map((course) => (
+                    <CourseCard key={course.id} course={course} />
+                ))}
+            </div>}
+        </>
+    )
+}
+
+export default CourseCards
