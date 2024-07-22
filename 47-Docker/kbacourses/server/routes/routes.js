27a1e364d86e9e667ef6de8a9d8ef9f8887f3cd4
nirajajshenoy <157449412+nirajajshenoy@users.noreply.github.com>
2024-07-22 10:06:13 +0530

    Add files via upload

---
+++ 47-Docker/kbacourses/server/routes/routes.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const router = express.Router();
+const courses = require("../models/courses");
+const verifyToken = require("../middleware/authMiddleware")
+
+
+router.get("/courses",verifyToken, async (req, res) => {
+  const details = await courses.find({});
+  res.json(details);
+});
+
+router.get("/courses/:id", async (req, res) => {
+  const courseId = req.params.id;
+  const details = await courses.findOne({ courseId: courseId }, { _id: 0 });
+  res.json(details);
+});
+
+router.post("/courses", async (req, res) => {
+  try {
+    const data = req.body;
+    const result = await courses.create(data);
+    res.status(201).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json();
+  }
+});
+
+
+// router.post("/courses", async (req, res) => {
+//   try {
+//     const data = req.body;
+//     console.log("DATAA ", data);
+//     const result = new courses(data);
+//     await result.save();
+//     res.status(201).json(result);
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json();
+//   }
+// });
+
+router.put("/courses/:id", async (req, res) => {
+  const data = req.body;
+  const courseId = data.courseId;
+  try {
+    const result = await courses.findOneAndUpdate(
+      { courseId: courseId },
+      req.body,
+      { new: true, runValidators: true }
+    );
+    if (!result) {
+      return res.status(404).send("Course not found");
+    }
+    res.send("Course updated successfully");
+  } catch (error) {
+    res.status(500).send("Server error");
+  }
+});
+
+router.delete("/courses/:id",verifyToken, async (req, res) => {
+  const courseId = req.params.id;
+  if(req.userType != 'admin'){
+    return res
+    .status(401)
+    .json({ error: "Authentication failed- password doesn't match" });
+  }
+  try {
+    const result = await courses.findOneAndDelete({ courseId: courseId });
+    if (!result) {
+      return res.status(404).send("Course not found");
+    }
+    res.send("Course deleted successfully");
+  } catch (error) {
+    res.status(500).send("Server error");
+  }
+});
+
+module.exports = router;
